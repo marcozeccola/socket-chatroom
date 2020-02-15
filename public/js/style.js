2b31541a730f84d3332cb4e8439e4574effda8c1
marcozeccola <marcozeccolas@gmail.com>
2020-02-15 22:56:26 +0100

    log in form style

---
+++ public/js/style.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('..button-collapse');
+     M.Sidenav.init(elems, options);
+  });
--- server/modules/verify.js
+++ server/modules/verify.js
@@ -2,7 +2,6 @@
       //se è uno stringa e se non ha spazi (.trim toglie gli spazi ad inizio e fine)
       return typeof str === 'string' && str.trim().length > 0;
   };
-
   module.exports = {
       verify
   };
